fc7cde9cf3ad8adae3eb89cd6200c828ac24e7df
newtbytes <186638664+Newtbytes@users.noreply.github.com>
2026-05-27 10:35:18 -0700

    chore: add binary op overflow tests

---
+++ tests/valid/binary_ops/basic/edge/sub_underflow.c
@@ -0,0 +1,5 @@
+int main(void) {
+    // FIXME: -2147483648 is parsed as negate +2147483648, which compiler cannot parse into an i32
+    /* return -2147483648 - 1; */
+    return -2147483647 - 2;
+}
