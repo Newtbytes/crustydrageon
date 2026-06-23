aa2fda74d6b104e1f12e760776d6404cf5c73319
newtbytes <186638664+Newtbytes@users.noreply.github.com>
2026-06-23 13:57:29 -0700

    refactor: implement marking parameters as leaf nodes

--- src/graph.rs
+++ src/graph.rs
@@ -1,33 +1,24 @@
 #[macro_export]
 macro_rules! visitor_trait {
-    // TODO: implement marking parameters as leaf nodes
-    ($trait_name:ident { $($name:ident $([$leaf:ident])? ( $($child:ident : $ty:ty),+ )),+ }) => {
+    ($trait_name:ident { $($name:ident ( $($(#[$leaf:ident])? $child:ident : $ty:ty),+ )),+ }) => {
         paste::paste! {
             pub trait [<Mut $trait_name>] {
-                $(visitor_trait!(@node $($leaf)? $name ( $($child : $ty),+ ));)+
+                $(visitor_trait!(@node $name ( $($(#[$leaf])? $child : $ty),+ ));)+
             }
         }
     };
-
-    (@node $name:ident ( $($child:ident : $ty:ty),+ )) => {
-        visitor_trait!(@branch $name ( $($child : $ty),+ ));
-    };
-
-    (@node leaf $name:ident ( $($child:ident : $ty:ty),+ )) => {
-        visitor_trait!(@leaf $name ( $($child : $ty),+ ));
-    };
-
-    (@branch $name:ident ( $($child:ident : $ty:ty),+ )) => {
+    (@node $name:ident ( $($(#[$leaf:ident])? $child:ident : $ty:ty),+ )) => {
         paste::paste! {
             fn [<visit_ $name>](&mut self, $($child: &mut $ty),+) {
-                $(MutVisitable::accept($child, self);)+
+                $(visitor_trait!(@accept $($leaf)? self $child);)+
             }
         }
     };
 
-    (@leaf $name:ident ( $($child:ident : $ty:ty),+ )) => {
-        paste::paste! {
-            fn [<visit_ $name>](&mut self, $($child: &mut $ty),+) {}
-        }
+    (@accept $self:ident $child:ident) => {
+        MutVisitable::accept($child, $self)
+    };
+
+    (@accept leaf $self:ident $child:ident) => {
     };
 }
